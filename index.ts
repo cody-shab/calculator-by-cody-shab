9327c1dc7ed27d71cf4539394121971bce8e17d2
Cody Shab <137870904+cody-shab@users.noreply.github.com>
2024-04-09 10:48:47 +0500

    simple calculator
    a simple calculator which I learnt from GAWM program

---
+++ index.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env code
+
+import inquirer from "inquirer";
+
+
+
+const answer = await inquirer.prompt([
+  { message: "Enter 1st num:", type: "number", name: "firstNum" },
+  { message: "Enter 2nd num:", type: "number", name: "secondNum" },
+  {
+    message: 'Select any operator',
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  }
+]);
+
+if (answer.operator === "Addition") {
+  console.log(answer.firstNum + answer.SecndNum);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNum - answer.SecndNum);
+}
+else if (answer.operator === "Multiplication") {
+  console.log(answer.firstNum * answer.SecndNum);
+}
+else if (answer.operator === "Division") {
+  console.log(answer.firstNum / answer.SecndNum);
+} else {
+  console.log("Please select valid operator");
+}
+
+
