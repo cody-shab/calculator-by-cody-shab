b2b3d8f452917480c6243e525e4037cb0af6498d
Cody Shab <137870904+cody-shab@users.noreply.github.com>
2024-04-21 10:20:08 +0500

    commit changes

--- index.ts
+++ index.ts
@@ -5,26 +5,26 @@ import inquirer from "inquirer";
 
 
 const answer = await inquirer.prompt([
-  { message: "Enter 1st num:", type: "number", name: "firstNum" },
-  { message: "Enter 2nd num:", type: "number", name: "secondNum" },
+   {message: "Enter 1st number:", type: "number", name: "first_Number"},
+   {message: "Enter 2nd number:", type: "number", name: "second_Number"},
   {
     message: 'Select any operator',
     type: "list",
     name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"]
   }
 ]);
 
 if (answer.operator === "Addition") {
-  console.log(answer.firstNum + answer.SecndNum);
+  console.log(answer.first_Number + answer.second_Number);
 } else if (answer.operator === "Subtraction") {
-  console.log(answer.firstNum - answer.SecndNum);
+  console.log(answer.first_Number - answer.second_Number);
 }
 else if (answer.operator === "Multiplication") {
-  console.log(answer.firstNum * answer.SecndNum);
+  console.log(answer.first_Number * answer.second_Number);
 }
 else if (answer.operator === "Division") {
-  console.log(answer.firstNum / answer.SecndNum);
+  console.log(answer.first_Number / answer.second_Number);
 } else {
   console.log("Please select valid operator");
 }
